489258ff84b1b3c8708fb45b1171ab62ce351bf5
Gino Pilotino <gunzip@25151.no-reply.drupal.org>
2009-06-29 21:05:25 +0000

    Changed: removed unused javascript.

--- smsbulk.js
+++ smsbulk.js
@@ -2,15 +2,6 @@
   Drupal.behaviors.smsbulk = function(context) {
     var $smstext = $('#edit-sms-text');
     var $keystrokes = $('#keystrokes span').eq(0).text('0 / ');
-    var limit = Number($('#keystrokes').attr('class'));
-    $smstext.bind('keypress', function(e) {
-      var key = (e.keyCode ? e.keyCode : e.which);
-      if ($smstext.val().length >= limit && (key != String.charCodeAt('\b'))) {
-        e.preventDefault();
-        return false;
-      }
-      return true;
-    });
     $smstext.bind('keyup', function(e) {
       var chars = $smstext.val().length;
       $keystrokes.text(chars + ' / ');
